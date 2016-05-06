f6b8229b441a607014de0ce1b26acd034abfa916
Zeke <leozeke@users.noreply.github.com>
2016-05-06 11:24:42 -0300

    Update Header.php

--- CodeIgniter/application/views/layout/Header.php
+++ CodeIgniter/application/views/layout/Header.php
@@ -3,24 +3,24 @@
 <head>
 	<html lang="es">
 	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title><?= $titulo ?> | Lista</title>
 
 	<!-- Bootstrap -->
-    <link href="<?= base_url() ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    	<link href="<?= base_url() ?>/assets/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 <body>
 
-			<nav class="navbar navbar-inverse">
-			  <div class="container-fluid">
-			    <ul class="nav navbar-nav">
-			      <li class="active"><a href="<?= base_url()?>">List</a></li>
-			      <li><a href="<?= base_url()?>/index.php/userlist/add">Add</a></li>
-			      <li><a href="<?= base_url()?>/index.php/welcome">How I made this?</a></li> 
-			    </ul>
-			  </div>
-			</nav>
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <ul class="nav navbar-nav">
+		      <li class="active"><a href="<?= base_url()?>">List</a></li>
+		      <li><a href="<?= base_url()?>/index.php/userlist/add">Add</a></li>
+		      <li><a href="<?= base_url()?>/index.php/welcome">How I made this?</a></li> 
+		    </ul>
+		  </div>
+		</nav>
 	<hr>
